1a0c1964a6ff8e74e477898e37bf2fb4174f6065
Tyler Le <tyler0308gm@gmail.com>
2025-02-17 17:09:53 -0800

    Finsihed exercies

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(stringResult) {
+    return stringResult.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // A year is a leap year if
+    //1. It is divisible by 4 and it is not diviisble by 100 unless it is also divibible 
 
+    if(year % 4 === 0){
+        if(year % 100 === 0){ // Check if it is a century year
+            if(year % 400 ===0){ // Check if it is also divisible by 400
+                // It is leap year
+                return true
+            }else{
+                return false
+            }
+        }
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
